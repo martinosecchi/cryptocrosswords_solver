b1a844db024b156401d0b276d353e89c111d0564
Martino Secchi <smart91k@gmail.com>
2017-11-26 23:49:24 +0100

    start on input reading and problem form

--- lw.py
+++ lw.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
+#coding: utf-8 
 import json
 import os, sys
+import re
 
 alphabet = [l for l in 'abcdefghijklmnopqrstuvwxyz']
 
@@ -67,12 +70,68 @@ class Trie(object):
             node = node.children[l]
         node.words[word] = word  
 
-tf = LeveledList(testwords)
-t = Trie(testwords)
+# t = Trie(testwords)
 
-def load_words():
+def load_english():
     filename = "words_dictionary.json"
     with open(filename,"r") as english_dictionary:
         valid_words = json.load(english_dictionary)
         return valid_words.keys()
+def load_italian():
+    filename = "../parole/parole.txt"
+    lines = []
+    with open(filename, "r") as f:
+        for line in f:
+            try:
+                word = line.decode('utf8')
+                word.replace(u'à', 'a')
+                word.replace(u'á', 'a')
+                word.replace(u'è', 'e')
+                word.replace(u'é', 'e')
+                word.replace(u'í', 'i')
+                word.replace(u'ì', 'i')
+                word.replace(u'ò', 'o')
+                word.replace(u'ó', 'o')
+                word.replace(u'ù', 'u')
+                word.replace(u'ú', 'u')
+                for l in word:
+                    if l not in alphabet + ['\n']:
+                        raise Exception(l.decode('utf8') + u' - not in alphabet')
+                lines.append(word.rstrip('\n').encode('utf8'))
+            except Exception as e:
+                print 'unicode error: ' + line.rstrip('\n'), str(e)
+    return lines
 
+it = load_italian()
+
+def validate(lines, n, m):
+    pattern = re.compile(r'[_]{1}|[0-9]{1,2}')
+    try:
+        assert len(lines) == m
+        for l in lines:
+            assert len(l) == n
+            for e in range(len(l)):
+                l[e] = l[e].rstrip('\n')
+                assert pattern.match(l[e]) is not None
+    except AssertionError as ae:
+        print 'Error in input: validate failed.', ae
+
+def parse_input():
+    filename = "input.txt"
+    lines = []
+    hints = {}
+    with open(filename, "r") as f:
+        n = int(f.readline())
+        m = int(f.readline())
+        for i in range(m):
+            lines.append(f.readline().split(','))
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            k,v = line.split(',')
+            hints[k] = v.rstrip('\n')
+    validate(lines, n, m)
+    return lines, hints
+
+square, hints = parse_input()
